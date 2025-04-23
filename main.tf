662b5c145c0587d7cfe39ac000f442a03b2801e2
klover22 <klovversolutions@hotmail.com>
2025-04-23 13:46:23 -0400

    add port 80

--- main.tf
+++ main.tf
@@ -126,6 +126,23 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
   to_port           = 443
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
   cidr_ipv4         = "0.0.0.0/0"
